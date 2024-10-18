ba9319bb11b30e70b37c5ed965906739232e1e6c
Andrey <hishi@Andrey.local>
2024-10-18 21:02:17 +0700

    step 1

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -4,6 +4,7 @@ import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import App from './components/App';
 import resources from './locales/index.js';
+import './index.css';
 
 const init = async () => {
   const i18n = i18next.createInstance();
