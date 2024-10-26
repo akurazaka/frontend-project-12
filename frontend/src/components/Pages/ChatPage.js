4b31084475d63019b077012c58643bc2664c5865
Andrey <hishi@Andrey.local>
2024-10-27 00:42:31 +0700

    fix

--- frontend/src/components/Pages/ChatPage.js
+++ frontend/src/components/Pages/ChatPage.js
@@ -1,77 +1,31 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  ButtonGroup, Col, ListGroup, Dropdown, Button, Spinner, Alert,
-} from 'react-bootstrap';
+import { Col, ListGroup, ButtonGroup, Dropdown, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import filter from 'leo-profanity';
-import useInstance from '../../utils/axios';
+import { apiService, useGetChannelsQuery, useGetMessagesQuery } from '../../store/apiService';
 import {
-  selectChannelById, selectChannelNames, setChannel, updateChannels,
-  toggleModalAdd, toggleModalDelete, toggleModalChange,
+  setChannel, toggleModalAdd, toggleModalDelete, toggleModalChange,
 } from '../../store/channelSlice';
 import ChatWindow from '../ChatWindow';
-import { addAllMessages } from '../../store/messageSlice';
 import AddButton from '../Buttons/AddButton';
 import ModalAddChannel from '../Modals/ModalAddChannel';
 import ModalRemoveChannel from '../Modals/ModalRemoveChannel';
 import ModalChangeChannelName from '../Modals/ModalChangeChannelName';
-import routes from '../../routes';
 
 const ChatPage = () => {
   const { t } = useTranslation();
-  const instance = useInstance();
   const dispatch = useDispatch();
   const channelId = useSelector((state) => state.channels.channelId);
   const showModal = useSelector((state) => state.channels.showModalAdd);
   const showModalDelete = useSelector((state) => state.channels.showModalDelete);
   const showModalChange = useSelector((state) => state.channels.showModalChange);
-  const channels = useSelector((state) => state.channels.data);
-  const existingNames = useSelector(selectChannelNames);
-  const selectedChannel = useSelector((state) => selectChannelById(state, channelId));
-
-  // Добавлено состояние для отслеживания загрузки и ошибок
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true); // Начинаем загрузку
-      try {
-        // Увеличьте задержку перед началом загрузки данных
-        await new Promise((resolve) => setTimeout(resolve, 500));
-        
-        const [channelsResponse, messagesResponse] = await Promise.all([
-          instance({ method: 'get', url: routes.api.channelsPath() }),
-          instance({ method: 'get', url: routes.api.messagesPath() }),
-        ]);
-
-        dispatch(updateChannels(channelsResponse.data));
-        dispatch(addAllMessages(messagesResponse.data));
-      } catch (error) {
-        console.error('Failed to fetch data:', error);
-        setError(t('error.fetchData')); // Отобразить ошибку
-      } finally {
-        setLoading(false); // Завершаем загрузку
-      }
-    };
-    
-    fetchData();
-  }, [dispatch, instance, t]);
+  const { data: channels = [] } = useGetChannelsQuery();
+  const { data: messages = [] } = useGetMessagesQuery();
 
   return (
     <div className="h-100 p-5">
       <div className="container overflow-hidden rounded shadow d-flex flex-column h-100">
-        {loading && (
-          <Spinner animation="border" role="status" className="m-auto">
-            <span className="visually-hidden">{t('loading')}</span>
-          </Spinner>
-        )}
-        {error && (
-          <Alert variant="danger" className="m-2">
-            {error}
-          </Alert>
-        )}
         <div className="row h-100 bg-white flex-md-row">
           <Col sm={4} className="col-4 border-end px-0 bg-light flex-column h-100 d-flex">
             <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
@@ -117,14 +71,13 @@ const ChatPage = () => {
             </ListGroup>
           </Col>
           <Col sm={8} className="col p-0 h-100">
-            {selectedChannel && <ChatWindow channel={selectedChannel} />}
+            <ChatWindow channelId={channelId} />
           </Col>
         </div>
         <ModalAddChannel
           t={t}
           show={showModal}
           onHide={() => dispatch(toggleModalAdd(false))}
-          existingChannelNames={existingNames}
         />
         <ModalRemoveChannel
           t={t}
@@ -135,7 +88,6 @@ const ChatPage = () => {
           t={t}
           show={showModalChange}
           onHide={() => dispatch(toggleModalChange(null))}
-          existingChannelNames={existingNames}
         />
       </div>
     </div>
---
+++ frontend/src/store/apiService.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import routes from '../../routes';
+
+export const apiService = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: routes.api.basePath }),
+  endpoints: (builder) => ({
+    getChannels: builder.query({
+      query: () => routes.api.channelsPath(),
+    }),
+    getMessages: builder.query({
+      query: () => routes.api.messagesPath(),
+    }),
+  }),
+});
+
+export const { useGetChannelsQuery, useGetMessagesQuery } = apiService;
